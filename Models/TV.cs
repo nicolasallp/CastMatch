43d3b4f6348635247101bd29f52e23e26bfc3d83
nicolasallp <nikolasalp@gmail.com>
2024-01-03 13:55:19 -0300

    feat: add tv shows

--- Models/TV.cs
+++ Models/TV.cs
@@ -7,5 +7,6 @@
         public string? Year { get; set; }
         public int Popularity { get; set; }
         public string? ImageUrl { get; set; }
+        public bool IsMovie { get; set; }
     }
 }
