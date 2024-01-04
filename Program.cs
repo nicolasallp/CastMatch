dcb5de1f5ff76c13b4cb3f39e29f4dc046459c05
nicolasallp <nikolasalp@gmail.com>
2024-01-04 11:08:17 -0300

    update: secret

--- Program.cs
+++ Program.cs
@@ -13,15 +13,15 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 
-var kvUri = builder.Configuration["KVUrl"];
+//var kvUri = builder.Configuration["KVUrl"];
 
-var credential = new ClientSecretCredential(
-        builder.Configuration["TenantId"],
-        builder.Configuration["ClientId"],
-        builder.Configuration["ClientSecretId"]
-    );
+//var credential = new ClientSecretCredential(
+//        builder.Configuration["TenantId"],
+//        builder.Configuration["ClientId"],
+//        builder.Configuration["ClientSecretId"]
+//    );
 
-var client = new SecretClient(new Uri(kvUri), credential);
+//var client = new SecretClient(new Uri(kvUri), credential);
 //ClientTvSecret.Secret = client.GetSecret(builder.Configuration["KeyVaultName"]).Value.Value;
 
 var app = builder.Build();
