165beac34c1a687c53a6315278bb7350adaba5f8
nicolasallp <nikolasalp@gmail.com>
2024-01-05 13:58:45 -0300

    update: classes.

--- Models/Match.cs
+++ Models/Match.cs
@@ -1,11 +1,12 @@
 ﻿namespace castmatch.Models
 {
-    public class TV
+    public class Match
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Year { get; set; }
         public int Popularity { get; set; }
+        public string[]? Character { get; set; }
         public string? ImageUrl { get; set; }
         public bool IsMovie { get; set; }
     }
